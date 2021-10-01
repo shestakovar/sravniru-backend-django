92de0e4706f960cb743427d71049b5c925b25ca3
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-10-01 19:29:35 +0400

    Add filters

--- sravniru/backend/managers.py
+++ sravniru/backend/managers.py
@@ -7,3 +7,15 @@ class ProposalManager(models.QuerySet):
 
     def sort_sum(self):
         return self.order_by('-rate__creditAmount__to', '-rate__creditAmount___from')
+
+    def filter_price(self, price):
+        return self.filter(rate__creditAmount___from__lte=price).exclude(rate__creditAmount__to__lt=price)
+
+    def filter_initial_amount(self, price, initialAmount):
+        percentage = int(initialAmount) / int(price) * 100
+        return self.filter(rate__initialAmount___from__lte=percentage).exclude(rate__initialAmount__to__lt=percentage)
+
+    def filter_term(self, term):
+        return self.annotate(minterm=models.Min('rate__periods__term___from')) \
+            .annotate(maxterm=models.Max('rate__periods__term__to')) \
+                .filter(minterm__lte=term).filter(maxterm__gte=term)
--- sravniru/backend/views.py
+++ sravniru/backend/views.py
@@ -9,10 +9,25 @@ class ProposalApiView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         list_proposals = self.queryset
-        query = self.request.query_params.get('sort')
-        if (query is not None):
-            if (query == 'rate'):
+
+        creditAmount = self.request.query_params.get('creditAmount')
+        if (creditAmount is not None and creditAmount.isdigit()):
+            list_proposals = list_proposals.filter_price(creditAmount)
+        
+        initialAmount = self.request.query_params.get('initialAmount')
+        if (creditAmount is not None and initialAmount is not None and creditAmount.isdigit() and initialAmount.isdigit()):
+            list_proposals = list_proposals.filter_initial_amount(creditAmount, initialAmount)
+        
+        term = self.request.query_params.get('term')
+        if (term is not None and term.isdigit()):
+            list_proposals = list_proposals.filter_term(term)
+
+
+
+        sort = self.request.query_params.get('sort')
+        if (sort is not None):
+            if (sort == 'rate'):
                 list_proposals = list_proposals.sort_rate()
-            if (query == 'sum'):
+            if (sort == 'sum'):
                 list_proposals = list_proposals.sort_sum()
         return list_proposals
